24036acc0aa46fe8d1ad39e2231340cc5d2586a7
dghuang <davidgao7@live.com>
2015-11-19 23:42:09 -0500

    Renamed folder to spring_mvc instead of spring mvc

--- Spring MVC/game_manager/src/main/java/com/dghuang/student_manager/api/MappingValues.java
+++
@@ -1,9 +0,0 @@
-package com.dghuang.student_manager.api;
-
-public class MappingValues {
-	
-	public static final String UNIVERSITIES = "/universities";
-	
-	public static final String STUDENTS = "/students";
-	
-}
--- Spring MVC/game_manager/src/main/java/com/dghuang/student_manager/repository/StudentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.dghuang.student_manager.repository;
-
-import org.springframework.data.repository.PagingAndSortingRepository;
-
-import com.dghuang.student_manager.entity.Student;
-
-public interface StudentRepository extends PagingAndSortingRepository<Student, String> {
-	
-}
